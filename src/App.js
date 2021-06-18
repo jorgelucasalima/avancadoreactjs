75354886ba7c24ed746cf98211e7626ff791135a
jorgelucasalima <jorgelucasalima@gmail.com>
2021-06-18 20:25:17 -0300

    pegando api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 
 // https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
+
+    const [nutri, setNutri] = useState([])
+
+    useEffect(() => {
+        function loadApi() {
+            let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+            
+            fetch(url)
+            .then((resultado) => resultado.json())
+            .then((json) => {
+                setNutri(json)
+            })
+
+
+        }
+
+        loadApi()
+    }, [])
+
+
+
     return(
-        <div>
-            Teste
+        <div className="container">
+            <header>
+                <strong>React Nutri</strong>
+            </header>
         </div>
 
     )
