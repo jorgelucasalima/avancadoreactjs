363731498093c0d9362994f9c197f9236615754d
jorgelucasalima <jorgelucasalima@gmail.com>
2021-06-18 19:53:26 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+// https://sujeitoprogramador.com/rn-api/?api=posts
+
+function App() {
+    return(
+        <div>
+            Teste
+        </div>
+
+    )
+}
+
+
+export default App
